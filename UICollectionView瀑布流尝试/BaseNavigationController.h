3260ed5f5a5f93baf38183a7a0ef053e7c4dc8ba
dingpuyu <771005227@qq.com>
2015-11-22 14:38:55 +0800

    添加了可右滑返回的控制器
    添加了可右滑返回的控制器

---
+++ UICollectionView瀑布流尝试/BaseNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  BaseNavigationController.h
+//  MoShouBroker
+//
+//  Created by xiaotei on 15/11/22.
+//  Copyright © 2015年 5i5j. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseNavigationController : UINavigationController
+
+@end
